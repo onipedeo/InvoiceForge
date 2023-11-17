6aac36d42b8d2970993858f0958e7498970c65ef
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-16 19:16:35 -0500

    move and update knexfile

---
+++ backend/db/knexfile.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.PGHOST,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+      port: process.env.PGPORT,
+      ssl: process.env.PGSSL,
+    },
+    migrations: {
+      directory: './migrations',
+      tableName: 'migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL + '?ssl=true',
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'migrations',
+    },
+  },
+
+};
