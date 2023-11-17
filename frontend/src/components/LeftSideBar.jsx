acab7571126454cc06d012432e8361299444a7ab
Tobi Onipede <onipede89@gmail.com>
2023-11-17 17:46:32 +0100

    Add leftside bar jsx structure

---
+++ frontend/src/components/LeftSideBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../styles/leftSideBar.scss'
+
+const LeftSideBar = () => {
+  return (
+    <div className='sidebar'>
+      {/* <h1>Hello left side bar</h1> */}
+      <table className='sidebar-list'>
+       <tr>
+        <th>Weeks</th>
+       </tr>
+       <tr>
+        <td>Jan 20 - Jan 27</td>
+       </tr>
+       
+       <tr>
+        <td>Jan 20 - Jan 27</td>
+       </tr>
+       
+       <tr>
+        <td>Jan 20 - Jan 27</td>
+       </tr>
+
+       <tr>
+        <td>Jan 20 - Jan 27</td>
+       </tr>
+
+       <tr>
+        <td>Jan 20 - Jan 27</td>
+       </tr>
+
+      </table>
+
+    </div>
+  );
+};
+
+export default LeftSideBar;
