3fea8253c04f700e25eabca8e6d22371363f0767
carolineolagunju <olamidejr21@gmail.com>
2023-11-17 00:27:17 -0700

    changed <li> to <td>

--- frontend/src/components/SimpleAppointmets.jsx
+++ frontend/src/components/SimpleAppointmets.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import "../styles/SimpleAppointments.scss"; // Import your CSS file for styling
 
 const appointments = [
-  { id: 1, appointment: "Tap repair" },
-  { id: 2, appointment: "Light fix" },
-  { id: 3, appointment: "Groceries Shopping" },
-  { id: 4, appointment: "Doctor's Appointment" },
-  { id: 5, appointment: "Go to the movies" },
+  { id: 1, appointment: "Tap repair", date: "2023-11-10", hours: 2 },
+  { id: 2, appointment: "Light fix", date: "2023-11-22", hours: 4 },
+  { id: 3, appointment: "Groceries Shopping", date: "2023-10-10", hours: 1 },
+  { id: 4, appointment: "Doctor's Appointment", date: "2024-01-2", hours: 4.5 },
+  { id: 5, appointment: "Go to the movies", date: "2024-02-03", hours: 6 },
 ];
 
 const SimpleAppointments = () => {
@@ -21,17 +21,33 @@ const SimpleAppointments = () => {
   };
 
   const appointmentList = appointments.map((appointment) => (
-    <li key={appointment.id} className="appointment-list-item">
-      {appointment.appointment}
+    <tr key={appointment.id} className="appointment-list-item">
+    <td>{appointment.appointment}</td>
+    <td>{appointment.date}</td>
+    <td>{appointment.hours} hours</td>
+    <td>
       <input
         type="checkbox"
         checked={checkedAppointments.includes(appointment.id)}
         onChange={() => handleAppointmentCheck(appointment.id)}
       />
-    </li>
+    </td>
+  </tr>
   ));
 
-  return <ul className="appointment-list">{appointmentList}</ul>;
+  return (
+    <table className="appointment-list">
+    <thead>
+      <tr>
+        <th>Appointment</th>
+        <th>Date</th>
+        <th>Hours</th>
+        <th>Checkbox</th>
+      </tr>
+    </thead>
+    <tbody>{appointmentList}</tbody>
+  </table>
+);
 };
 
 export default SimpleAppointments;
