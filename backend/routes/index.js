f41565d10521f8428c56767c3075832384b0fb8c
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-17 16:20:28 -0500

    setup api router to use subrouters and morgan

---
+++ backend/routes/index.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const morgan = require('morgan');
+
+// Import sub routers
+const user = require('./userRoutes');
+const client = require('./clientRoutes');
+const appointment = require('./appointmentRoutes');
+const invoice = require('./invoiceRoutes');
+const address = require('./addressRoutes');
+
+const router = express.Router();
+
+router.use(morgan('dev'));
+router.use('/user', user);
+router.use('/client', client);
+router.use('/appointment', appointment);
+router.use('/invoice', invoice);
+router.use('/address', address);
+
+// test GET route
+router.get('/test', (req, res) => res.json({
+  message: "Seems to work!",
+}));
+
+
+
+
+
+module.exports = router;
