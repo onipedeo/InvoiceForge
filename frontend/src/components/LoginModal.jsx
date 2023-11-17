6ad6566d8a956fc60d48e95a630985539e2986dd
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-11-17 16:39:32 -0500

    add close button

--- frontend/src/components/LoginModal.jsx
+++ frontend/src/components/LoginModal.jsx
@@ -2,27 +2,33 @@ import  "../styles/login-modal.scss"
 
 export default function LoginModal(props) {
 
-  return ( <div className="login-container">
-  <h2>Login</h2>
-  <form className="login-form ">
-    <div className="form-group">
-      <label htmlFor="email">Email:</label>
-      <input
-        type="email"
-        id="email"
-        required
-      />
+  return (
+    <div className="login-container">
+      <div className="modal" id="loginModal">
+        <div className="modal-content">
+          <span className="close">&times;</span>
+          <h2>Login</h2>
+          <form className="login-form ">
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                required
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
     </div>
-    <div className="form-group">
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        id="password"
-        required
-      />
-    </div>
-    <button type="submit">Login</button>
-  </form>
-</div>
-)
+  );
 }
