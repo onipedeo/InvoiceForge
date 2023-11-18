20d0676524237a5ee3be53bdc28d0e56c00c65d2
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-18 14:24:55 -0500

    setup update Address

--- frontend/src/apiRequests/updating/updateAddress.jsx
+++ frontend/src/apiRequests/updating/updateAddress.jsx
@@ -1,12 +1,6 @@
 import put from '../helpers/put';
-/**
- *
- * @param {*} addressData
- * @returns promise to update addressId
- *
- * @description
- * This function is used to update an address.
- */
-export const createAddress = (addressData) => {
-  return put('/api/address', addressData);
+
+
+export const updateAddress = (addressId, addressData) => {
+  return put(`/api/address/${addressId}`, addressData);
 };
