98780c893b7f1057b6686152df9c34b022fe8fad
carolineolagunju <olamidejr21@gmail.com>
2023-11-17 18:32:28 -0700

    fix conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,28 @@
-<<<<<<< HEAD
-import { useState } from 'react';
+
+
 import './App.css';
-import LandingPage from './components/Landingpage';
+
 import SimpleAppointments from './components/SimpleAppointmets'
 import InvoiceGeneratedModal from './components/InvoiceGeneratedModal'
 import InvoicePreview from './components/InvoicePreview';
 import { PDFViewer } from '@react-pdf/renderer';
-=======
+
 import { useState } from 'react'
 import './App.css'
 import LandingPage from './components/Landingpage'
-<<<<<<< HEAD
-=======
+
 import TopNavBar from './components/TopNavBar'
-<<<<<<< HEAD
->>>>>>> skeleton/top-navbar
-=======
+
+
 import Footer from './components/footer'
->>>>>>> skeleton/footer
->>>>>>> master
+
+
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
+
     {/* <PDFViewer>
       <InvoicePreview/>
     </PDFViewer> */}
@@ -33,16 +31,12 @@ function App() {
 
       {/* <SimpleAppointments/> */}
       
-=======
-<<<<<<< HEAD
-      <LandingPage />
-=======
+
      < TopNavBar/>
       <LandingPage />
       <Footer/>
      
->>>>>>> skeleton/top-navbar
->>>>>>> master
+
     </>
   )
 }
