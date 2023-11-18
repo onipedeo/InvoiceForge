1af486494b7b4f55f105d3de69eb97294f2e4fe3
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-18 13:37:02 -0500

    streamline getUserData with updated backend

--- frontend/src/apiRequests/retrieval/getUserData.jsx
+++ frontend/src/apiRequests/retrieval/getUserData.jsx
@@ -1,12 +1,8 @@
 import get from '../helpers/get.jsx';
 
 export const getUserData = async (userId) => {
-  const user = await get(`/api/user/${userId}`);
-  const address = await get(`/api/address/${user.addressId}}`);
-  const clients = await get(`/api/user/${userId}/clients`);
-  const appointments = await get(`/api/user/${userId}/appointments`);
-  const invoices = await get(`/api/user/${userId}/invoices`);
-  const notInvoiced = await get(`/api/user/${userId}/not-invoiced`);
-  const unreviewed = await get(`/api/user/${userId}/unreviewed`);
-  return { user, address, clients, appointments, invoices, notInvoiced, unreviewed };
+  //exctract userId and addressId from user object
+
+  const { address, clients, appointments, invoices, notInvoiced, unreviewed }  = await get(`/api/user/${userId}`);
+  return { address, clients, appointments, invoices, notInvoiced, unreviewed };
 };
