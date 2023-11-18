f927da96bb29b7290e9da386a1add75bd6d49250
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-18 01:00:46 -0500

    clarify documentation, edit route path for clarity

--- backend/routes/invoiceRoutes.js
+++ backend/routes/invoiceRoutes.js
@@ -2,13 +2,13 @@ const invoiceController = require('../controllers/invoice');
 const express = require('express');
 const router = express.Router();
 
-// POST create invoice
+// POST api/invoice/ - create invoice
 router.post('/', (req, res) => {
   invoiceController.create(req, res);
 });
 
-// PUT set paid = true
-router.put('/:id', (req, res) => {
+// PUT api/invoice/id/paid - set paid = true
+router.put('/:id/paid', (req, res) => {
   invoiceController.setPaid(req, res);
 });
 module.exports = router;
