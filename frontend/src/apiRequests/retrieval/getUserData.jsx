dc24104b07138aae1ae39dc831c9de9cdae7673a
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-18 14:44:57 -0500

    adjust name to fit convention

--- frontend/src/apiRequests/retrieval/getUserData.jsx
+++ frontend/src/apiRequests/retrieval/getUserData.jsx
@@ -1,8 +1,6 @@
 import get from '../helpers/get.jsx';
 
 export const getUserData = async (userId) => {
-  //exctract userId and addressId from user object
-
   const { address, clients, appointments, invoices, notInvoiced, unreviewed }  = await get(`/api/user/${userId}`);
   return { address, clients, appointments, invoices, notInvoiced, unreviewed };
 };
