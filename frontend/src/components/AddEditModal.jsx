0c9e22a455c4edd2712300884c2a79e3d02ef447
Tobi Onipede <onipede89@gmail.com>
2023-11-18 03:09:49 +0100

    work in progress css for add-Edit Modal

--- frontend/src/components/AddEditModal.jsx
+++ frontend/src/components/AddEditModal.jsx
@@ -4,51 +4,50 @@ import '../styles/addEditModal.scss';
 const AddEditModal = () => {
   return (
     <div className='modal'>
-      {/* <h2>Hello Add/Edit Modal</h2> */}
-      <div className="appointment-modal">
-        <form >
-          <h2>Book an Appointment</h2>
-          <button className="close-modal" type='submit'>❌</button>
-          <label htmlFor="Title">
-            Title:
-          </label>
-          <input
-            type="text"
-            name="title"
-          />
-          <label>
-            Start Time:
-          </label>
-          <input
-            type="datetime-local"
-            name="startTime"
-          />
-          <label>
-            End Time:
-          </label>
-          <input
-            type="datetime-local"
-            name="endTime"
-          />
-          <label>
-            Appointment Rate :
-          </label>
-          <select>
-            <option value=""></option>
-            <option value="standard">Standard</option>
-            <option value="pro">Pro</option>
-            <option value="premium">Premium</option>
-          </select>
-          <label>
-            Notes:
-          </label>
-          <textarea
-            name="notes"
-          />
-          <br />
-          <button type="submit">Save Appointment</button>
-        </form>
-      </div>
+      <h2>Book an Appointment</h2>
+
+      <form >
+        <button className="close-modal" type='submit'>❌</button>
+        <label htmlFor="Title">
+          Title:
+        </label>
+        <input
+          type="text"
+          name="title"
+        />
+        <label>
+          Start Time:
+        </label>
+        <input
+          type="datetime-local"
+          name="startTime"
+        />
+        <label>
+          End Time:
+        </label>
+        <input
+          type="datetime-local"
+          name="endTime"
+        />
+        <label>
+          Appointment Rate :
+        </label>
+        <select>
+          <option value=""></option>
+          <option value="standard">Standard</option>
+          <option value="pro">Pro</option>
+          <option value="premium">Premium</option>
+        </select>
+        <label>
+          Notes:
+        </label>
+        <textarea
+          name="notes"
+        />
+        <br />
+        <button type="submit">Save Appointment</button>
+      </form>
+
     </div>
   );
 };
