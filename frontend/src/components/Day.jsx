41f372180cccacdc42661954b7f71b3c0c751e89
Tobi Onipede <onipede89@gmail.com>
2023-11-18 02:30:29 +0100

    refactor with fake data and render each day side by sde

--- frontend/src/components/Day.jsx
+++ frontend/src/components/Day.jsx
@@ -9,127 +9,21 @@ const day = ["Sunday", "Monday", "Tuesday", "wednessday", "Thursday", "Friday", 
 const dayList = day.map((day, index) => {
 
   return (
-    <div key={index} className='day-container'>
-      <div className='day-housing'>
-        {/* <h4>Hello Day Component</h4> */}
-        <div className='single-day'>
-          <div className='days-bar'>
-            {day}
-          </div>
-          <Appointments />
-        </div>
+    <div key={index} className='day-housing'>
+      <div>
+        {day}
       </div>
+      <Appointments />
     </div>
   )
 })
 
 const Day = () => {
   return (
-    <div className='day-container'>
+    <>
       {dayList}
-      {/* <div className='day-housing'>
-       
-        <div className='single-day'>
-          <div className='days-bar'>
-            Sunday
-          </div>
-          <Appointments />
-        </div>
-        <div className='single-day'>
-          <div className='days-bar'>
-            Monday
-          </div>
-          <div>
-            <div className='add-edit'>
-              <h4>time</h4>
-              <div>
-                <button type='submit'>❌</button>
-                <button>✏️</button>
-              </div>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut at voluptates dolorem dolorum, tenetur minus accusantium, debitis, mollitia amet voluptatum perspiciatis enim sit non cum est nulla! At, minus quasi.</p>
-          </div>
-        </div>
-        <div className='single-day'>
-          <div className='days-bar'>
-            Tuesday
-          </div>
-          <div>
-            <div className='add-edit'>
-              <h4>time</h4>
-              <div>
-                <button type='submit'>❌</button>
-                <button>✏️</button>
-              </div>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut at voluptates dolorem dolorum, tenetur minus accusantium, debitis, mollitia amet voluptatum perspiciatis enim sit non cum est nulla! At, minus quasi.</p>
-          </div>
-        </div>
-        <div className='single-day'>
-          <div className='days-bar'>
-            Wednessday
-          </div>
-          <div>
-            <div className='add-edit'>
-              <h4>time</h4>
-              <div>
-                <button type='submit'>❌</button>
-                <button>✏️</button>
-              </div>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut at voluptates dolorem dolorum, tenetur minus accusantium, debitis, mollitia amet voluptatum perspiciatis enim sit non cum est nulla! At, minus quasi.</p>
-          </div>
-        </div>
-        <div className='single-day'>
-          <div className='days-bar'>
-            Thursday
-          </div>
-          <div>
-            <div className='add-edit'>
-              <h4>time</h4>
-              <div>
-                <button type='submit'>❌</button>
-                <button>✏️</button>
-              </div>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut at voluptates dolorem dolorum, tenetur minus accusantium, debitis, mollitia amet voluptatum perspiciatis enim sit non cum est nulla! At, minus quasi.</p>
-          </div>
-        </div>
-        <div className='single-day'>
-          <div className='days-bar'>
-            Friday
-          </div>
-          <div>
-            <div className='add-edit'>
-              <h4>time</h4>
-              <div>
-                <button type='submit'>❌</button>
-                <button>✏️</button>
-              </div>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut at voluptates dolorem dolorum, tenetur minus accusantium, debitis, mollitia amet voluptatum perspiciatis enim sit non cum est nulla! At, minus quasi.</p>
-          </div>
-        </div>
-        <div className='single-day'>
-          <div className='days-bar'>
-            Saturday
-          </div>
-          <div>
-            <div className='add-edit'>
-              <h4>time</h4>
-              <div>
-                <button type='submit'>❌</button>
-                <button>✏️</button>
-              </div>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut at voluptates dolorem dolorum, tenetur minus accusantium, debitis, mollitia amet voluptatum perspiciatis enim sit non cum est nulla! At, minus quasi.</p>
-          </div>
-        </div>
+    </>
 
-      </div> */}
-
-
-    </div>
 
   );
 };
