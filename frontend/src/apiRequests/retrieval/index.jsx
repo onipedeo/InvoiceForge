a58e901bfa70ca2ad1013cbd9c784995a605bbef
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-18 15:16:13 -0500

    set up indicies for retrieval and updating

---
+++ frontend/src/apiRequests/retrieval/index.jsx
@@ -0,0 +1,9 @@
+import getAppointmentData from './getAppointmentData';
+import getClientData from './getClientData';
+import getUserData from './getUserData';
+
+export {
+  getAppointmentData,
+  getClientData,
+  getUserData,
+};
---
+++ frontend/src/apiRequests/updating/index.jsx
@@ -0,0 +1,15 @@
+import { confirmAppointmentHours } from "./ConfirmAppointmentHours";
+import { updateAddress } from "./updateAddress";
+import { updateAppointment } from "./updateAppointment";
+import { updateClient } from "./updateClient";
+import { updateUser } from "./updateUser";
+import { setInvoicePaid } from "./setInvoicePaid";
+
+export {
+  confirmAppointmentHours,
+  updateAddress,
+  updateAppointment,
+  updateClient,
+  updateUser,
+  setInvoicePaid,
+};
