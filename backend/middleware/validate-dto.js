e3876ee8e9d727a303d9f7d8919530062e0392ce
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-17 21:39:10 -0500

    setup validation middleware

---
+++ backend/middleware/validate-dto.js
@@ -0,0 +1,13 @@
+function validateDto(schema) {
+  return async (req, res, next) => {
+    try {
+      const validatedBody = await schema.validate(req.body);
+      req.body = validatedBody;
+      next();
+    } catch (error) {
+      res.status(422).json({ error });
+    }
+  };
+}
+
+module.exports = validateDto;
