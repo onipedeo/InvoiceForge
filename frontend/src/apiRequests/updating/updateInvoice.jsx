abb754e89f878b5f6b4b50f59d555d36d9f8aef9
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-18 15:16:44 -0500

    remove depricated update invoice

--- frontend/src/apiRequests/updating/updateInvoice.jsx
+++
@@ -1,6 +0,0 @@
-import create from '../helpers/create';
-
-// updating appointments is handled by the api. Simply include an array "appointmentIds" in the invoiceData object.
-export const createInvoice = async (invoiceData) => {
-  return await create('/api/invoice', invoiceData);
-};
