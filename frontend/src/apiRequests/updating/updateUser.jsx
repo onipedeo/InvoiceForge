dcf2af7238c033b67b0b91e1599e0366daac60ba
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-18 15:18:07 -0500

    update documentation

--- frontend/src/apiRequests/updating/updateUser.jsx
+++ frontend/src/apiRequests/updating/updateUser.jsx
@@ -2,7 +2,7 @@ import put from '../helpers/put';
 /**
  *
  * @param {obj} userData contains the form data for creating a new user
- * @returns promise to new userId
+ * @returns new userId
  */
 export const createUser = async (userId, userData) => {
   return await put(`/api/user/${userId}`, userData);
