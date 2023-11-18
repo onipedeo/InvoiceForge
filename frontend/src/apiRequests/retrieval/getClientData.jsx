f3ec26f92ddf50080fd17f5e61f19ed9e86e440a
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-18 14:18:00 -0500

    simply getClientData

--- frontend/src/apiRequests/retrieval/getClientData.jsx
+++ frontend/src/apiRequests/retrieval/getClientData.jsx
@@ -1,25 +1,8 @@
 import get from "../helpers/get";
 
 export const getClientData = async (clientId) => {
-  const client = await get(
+  const { client, address, appointments, invoices, invoiced, reviewed, notReviewed} = await get(
     `/api/client/${clientId}`
   );
-
-  const address = await get(
-    `api/address/${client.addressId}`
-  );
-
-  const notInvoiced = await get(
-    `/api/client/${clientId}/not-invoiced`
-  );
-
-  const unreviewed = await get(
-    `/api/client/${clientId}/unreviewed`
-  );
-
-  const invoices = await get(
-    `/api/client/${clientId}/invoices`
-  );
-
-  return { notInvoiced, unreviewed, invoices, client, address };
+  return { appointments, invoiced, reviewed, notReviewed, invoices, client, address };
 };
