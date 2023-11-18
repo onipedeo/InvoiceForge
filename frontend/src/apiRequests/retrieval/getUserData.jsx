24e2a5b6700c6c69d9610ee2eaaa7c9bfe890db0
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-18 12:19:56 -0500

    add user and address to data return

--- frontend/src/apiRequests/retrieval/getUserData.jsx
+++ frontend/src/apiRequests/retrieval/getUserData.jsx
@@ -1,10 +1,12 @@
 import get from '../helpers/get.jsx';
 
 export const getUserData = async (userId) => {
+  const user = await get(`/api/user/${userId}`);
+  const address = await get(`/api/address/${user.addressId}}`);
   const clients = await get(`/api/user/${userId}/clients`);
   const appointments = await get(`/api/user/${userId}/appointments`);
   const invoices = await get(`/api/user/${userId}/invoices`);
   const notInvoiced = await get(`/api/user/${userId}/not-invoiced`);
   const unreviewed = await get(`/api/user/${userId}/unreviewed`);
-  return { clients, appointments, invoices, notInvoiced, unreviewed };
-}
+  return { user, address, clients, appointments, invoices, notInvoiced, unreviewed };
+};
