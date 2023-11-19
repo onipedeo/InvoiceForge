9ddc1efb03c8b4cc6892bcec8b5c011ba0e6968b
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-19 17:48:19 -0500

    setup get to handle req.bodys

--- frontend/src/apiRequests/helpers/get.jsx
+++ frontend/src/apiRequests/helpers/get.jsx
@@ -3,8 +3,15 @@
  * @param {string} url
  * @returns promise with data
  */
-export const get = (url) => {
-  return fetch(url)
-    .then(response => response.json())
-    .catch(err => console.log(err));
+export const get = (url, body) => {
+
+  const options =  {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body) || {},
+  }
+
+return fetch(url, (options))
+  .then(response => response.json())
+  .catch(err => console.log(err));
 };
