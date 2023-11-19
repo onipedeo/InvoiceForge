3cf28b269f3d833ef886ab08a164837eb39f00c4
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-11-18 22:48:13 -0500

    find user data by email input

--- frontend/src/components/LoginModal.jsx
+++ frontend/src/components/LoginModal.jsx
@@ -1,6 +1,34 @@
-import  "../styles/login-modal.scss"
+import React, { useState } from 'react';
 
-export default function LoginModal(props) {
+import "../styles/login-modal.scss";
+
+const LoginModal = (props) => {
+  const [email, setEmail] = useState('');
+  const [userData, setUserData] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+     
+      const response = await fetch(`/api/user/${email}`);
+
+      if (response.ok) {
+        // Parse the response JSON
+        const data = await response.json();
+
+        // Handle the response
+        const { userData } = data;
+        // Set token or user ID in your application state or context
+        setUserData(userData);
+        
+      } else {
+        console.error('Login failed:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  };
 
   return (
     <div className="login-container">
@@ -8,12 +36,14 @@ export default function LoginModal(props) {
         <div className="modal-content">
           <span className="close">&times;</span>
           <h2>Login</h2>
-          <form className="login-form ">
+          <form className="login-form" onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="email">Email:</label>
               <input
                 type="email"
                 id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
               />
             </div>
@@ -22,7 +52,7 @@ export default function LoginModal(props) {
               <input
                 type="password"
                 id="password"
-                required
+                // required
               />
             </div>
             <button type="submit">Login</button>
@@ -31,4 +61,7 @@ export default function LoginModal(props) {
       </div>
     </div>
   );
-}
+};
+
+export default LoginModal;
+
