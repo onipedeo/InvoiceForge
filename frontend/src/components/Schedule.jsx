c70916518df216c17261a22554bdd7bede828463
Tobi Onipede <onipede89@gmail.com>
2023-11-20 04:34:56 +0100

    change location of the add schedule button

--- frontend/src/components/Schedule.jsx
+++ frontend/src/components/Schedule.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React  from 'react';
 import '../styles/schedule.scss';
 import Day from './Day';
 
+
+
 const Schedule = (props) => {
+  
   return (
     <div className='full-scheduler'>
       <Day />
-      <button id='floating-add-button'>➕</button>
     </div>
   );
 };
