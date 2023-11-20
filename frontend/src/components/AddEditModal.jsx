e02efac2d9a6b1da9f9be1b3166d3284ea929da1
Tobi Onipede <onipede89@gmail.com>
2023-11-20 04:33:52 +0100

    implement open and close modal logic

--- frontend/src/components/AddEditModal.jsx
+++ frontend/src/components/AddEditModal.jsx
@@ -2,53 +2,58 @@ import React from 'react';
 import '../styles/addEditModal.scss';
 
 const AddEditModal = (props) => {
+  
+
   return (
-    <div className='modal'>
-      <h2>Book an Appointment</h2>
+    props.isOpen && (
 
-      <form >
-        <button className="close-modal" type='submit'>❌</button>
-        <label htmlFor="Title">
-          Title:
-        </label>
-        <input
-          type="text"
-          name="title"
-        />
-        <label>
-          Start Time:
-        </label>
-        <input
-          type="datetime-local"
-          name="startTime"
-        />
-        <label>
-          End Time:
-        </label>
-        <input
-          type="datetime-local"
-          name="endTime"
-        />
-        <label>
-          Appointment Rate :
-        </label>
-        <select>
-          <option value=""></option>
-          <option value="standard">Standard</option>
-          <option value="pro">Pro</option>
-          <option value="premium">Premium</option>
-        </select>
-        <label>
-          Notes:
-        </label>
-        <textarea
-          name="notes"
-        />
-        <br />
-        <button type="submit">Save Appointment</button>
-      </form>
+      <div className='modal'>
+        <h2>Book an Appointment</h2>
 
-    </div>
+        <form >
+          <button className="close-modal" type='submit' onClick={props.onClose}>❌</button>
+          <label htmlFor="Title">
+            Title:
+          </label>
+          <input
+            type="text"
+            name="title"
+          />
+          <label>
+            Start Time:
+          </label>
+          <input
+            type="datetime-local"
+            name="startTime"
+          />
+          <label>
+            End Time:
+          </label>
+          <input
+            type="datetime-local"
+            name="endTime"
+          />
+          <label>
+            Appointment Rate :
+          </label>
+          <select>
+            <option value=""></option>
+            <option value="standard">Standard</option>
+            <option value="pro">Pro</option>
+            <option value="premium">Premium</option>
+          </select>
+          <label>
+            Notes:
+          </label>
+          <textarea
+            name="notes"
+          />
+          <br />
+          <button type="submit" >Save Appointment</button>
+        </form>
+
+      </div>
+    )
   );
 };
 
