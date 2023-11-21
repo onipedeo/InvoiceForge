07c615de56c324920ce622b2c8c4aa077e0b9cf0
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-21 02:00:42 -0500

    fix knexfile

--- backend/db/knexfile.js
+++ backend/db/knexfile.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
-
+require('dotenv').config({path: '../.env'});
+const types = require('pg').types;
+// override parsing date column to Date()
+types.setTypeParser(1082, val => val);
 module.exports = {
 
   development: {
@@ -10,33 +12,14 @@ module.exports = {
       password: process.env.PG_PASSWORD,
       database: process.env.PG_DATABASE,
       port: process.env.PG_PORT || 5432,
-      ssl: process.env.PG_SSL,
+      ssl: process.env.PG_SSL ? { rejectUnauthorized: false } : false,
     },
     migrations: {
-      directory: './db/migrations',
+      directory: './migrations',
       tableName: 'migrations',
     },
     seeds: {
-      directory: './db/seeds',
-    },
-  },
-
-  test: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.PG_HOST,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE,
-      port: process.env.PG_PASSWORD || 5432,
-      ssl: process.env.PG_SSL,
-    },
-    migrations: {
-      directory: './db/migrations',
-      tableName: 'migrations',
-    },
-    seeds: {
-      directory: './db/seeds',
+      directory: './seeds',
     },
   },
 
