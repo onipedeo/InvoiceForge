f41c922cb6953ecfbc5f88f009a3ed469ff1c324
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-11-21 18:32:07 -0500

    fecth user id for login

--- frontend/src/components/LoginModal.jsx
+++ frontend/src/components/LoginModal.jsx
@@ -1,57 +1,55 @@
-import React, { useState } from 'react';
-
-import "../styles/login-modal.scss";
+import { useState, useEffect } from 'react';
+import requests from '../api/requests';
+import '../styles/login-modal.scss';
 
 const LoginModal = (props) => {
   const [email, setEmail] = useState('');
-  const {setUserData} = props
+  const { setUserId } = props;
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-     
-      const response = await fetch(`/api/user/${email}`);
-
-      if (response.ok) {
-        const data = await response.json();
-        const { userData } = data;
-        setUserData(userData);
-        
-      } else {
-        console.error('Login failed:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Login failed:', error);
-    }
+  const fetchData = () => {
+    requests
+      .get
+      .idByEmail(email)
+      .then((user) => {
+        setUserId(user.id);
+      });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [setUserId]);
 
   return (
     <div className="login-container">
       <div className="modal" id="loginModal">
-      <span className="close" onClick={props.onClose}>&times;</span>
+        <span className="close" onClick={props.onClose}>
+          &times;
+        </span>
         <div className="modal-content">
           <h2>Login</h2>
-          <form className="login-form" onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="email">Email:</label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
+          <form onSubmit={handleSubmit}>
+            <div className="login-form">
+              <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" />
+              </div>
+              <button type="submit">Login</button>
             </div>
-            <div className="form-group">
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                id="password"
-                // required
-              />
-            </div>
-            <button type="submit">Login</button>
           </form>
         </div>
       </div>
@@ -60,4 +58,6 @@ const LoginModal = (props) => {
 };
 
 export default LoginModal;
+
+
 
