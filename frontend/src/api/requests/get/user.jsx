5b82866c26a33d073db1e9cc3e54b3010df5aa34
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-22 05:07:51 -0500

    nest address inside of client and user, nest appointments inside of invoices, make sure that it returns the data with the object not just the id.

---
+++ frontend/src/api/requests/get/user.jsx
@@ -0,0 +1,33 @@
+import get from "../helpers";
+class UserData {
+  constructor(id) {
+    this.id = id;
+  }
+
+  appointments() {
+    return get(`/api/user/${this.id}/appointments`);
+  }
+
+  object() {
+    return get(`/api/user/${this.id}`);
+  }
+  clients() {
+    return get(`/api/user/${this.id}/clients`);
+  }
+  reviewed() {
+    return get(`/api/user/${this.id}/reviewed`);
+  }
+  unreviewed() {
+    return get(`/api/user/${this.id}/unreviewed`);
+  }
+  invoices() {
+    return get(`/api/user/${this.id}/invoiced`);
+  }
+
+};
+
+
+
+export default function(id) {
+  return new UserData(id);
+}
