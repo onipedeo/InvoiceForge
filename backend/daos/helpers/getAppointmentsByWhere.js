e4b0cc5a67e18a10dc33022ba4203a00a08a6e7f
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-22 14:33:14 -0500

    leave id's in instead

--- backend/daos/helpers/getAppointmentsByWhere.js
+++ backend/daos/helpers/getAppointmentsByWhere.js
@@ -2,11 +2,6 @@ const db = require('../../db/db');
 
 
 module.exports = async function (where) {
-  const sanitizeAppointments = (appointments) => {
-    return appointments.map((appointment) => {
-      const { invoice_id, user_id, client_id, ...rest } = appointment;
-      return rest;
-    });
-  };
-  return sanitizeAppointments(await db('appointments').where(where));
+
+  return await db('appointments').where(where);
 };
