d825936c0d2c3e2d2d7ad150c7fa5e187bdd7229
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-11-21 22:47:00 -0500

    create ClientList component

---
+++ frontend/src/components/ClientList.jsx
@@ -0,0 +1,11 @@
+import "../styles/client-list.scss"
+
+export default function ClientList() {
+  return (
+    <div className="client-list-container">
+      
+        <p>Client List Content</p>
+  
+    </div>
+  );
+}
