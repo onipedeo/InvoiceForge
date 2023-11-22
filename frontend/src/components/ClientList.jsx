8b6a6b46df8f0ac143bd745d966d53d53a0b3fec
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-11-22 16:50:29 -0500

    pass NewClientModel in and conditional rendering the model

--- frontend/src/components/ClientList.jsx
+++ frontend/src/components/ClientList.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import "../styles/client-list.scss"
+import NewClientModal from "./NewClientModal";
 
 export default function ClientList() {
+
+  const [isClientModalOpen, setClientModelOpen] = useState(false);
+
+ const handleNewClientModalClick = () => {
+     setClientModelOpen(true)
+  };
+
   return (
     <div className="client-list-container">
       
         <p>Client List Content</p>
-  
+        <button onClick={handleNewClientModalClick}>Add New Client</button>
+        {isClientModalOpen && <NewClientModal setClientModelOpen={setClientModelOpen}/>}
     </div>
   );
-}
+  }
