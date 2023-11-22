9a2bd63ddbc08b697d4aee8e7a41cbd1c3f4b387
carolineolagunju <olamidejr21@gmail.com>
2023-11-21 23:03:05 -0700

    Removed redundant imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,16 @@ import AppointmentContainer from "./components/AppointmentContainer";
 import { useState, useEffect } from "react";
 import "./App.css";
 import LandingPage from "./components/Landingpage";
-import requests from './api/requests'
+import TopNavBar from "./components/TopNavBar";
+import Footer from "./components/footer";
 
-import TopNavBar from './components/TopNavBar'
-import Footer from './components/footer'
-import requests from './api/requests'
+
 function App() {
-  const [user, setUser] = useState(null)
-
- 
+  const [user, setUser] = useState(null);
 
   return (
     <>
-      
-     < TopNavBar user={user} setUser={setUser}/>
+      <TopNavBar user={user} setUser={setUser} />
       <LandingPage />
       <Footer />
       {/* <AppointmentContainer userId={user.id} standardRateCents={user.standard_rate_cents}/> */}
