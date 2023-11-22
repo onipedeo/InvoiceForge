fa90141f456a79bc94c2850ab75558a8920c50c4
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-11-22 14:47:10 -0500

    logic if redirect

--- frontend/src/components/LoginModal.jsx
+++ frontend/src/components/LoginModal.jsx
@@ -1,26 +1,49 @@
 import { useState, useEffect } from 'react';
 import requests from '../api/requests';
 import '../styles/login-modal.scss';
+import ClientList from './ClientList';
 
 const LoginModal = (props) => {
   const [email, setEmail] = useState('');
-  const {setUser} = props;
+  const { setUser, user, setShowClientList, showClientList } = props;
+  const [client, setClient] = useState([]);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-  const fetchUser = () => {
-    requests
-      .get
-      .idByEmail(email)
-      .then((user) => {
-        setUser(user);
-        console.log(user);
-      });
+  useEffect(() => {
+    // This effect runs after every render
+    if (loggedIn && client.length === 0) {
+      console.log('client', client)
+      toggleShowClientList();
+    }
+  },[]); // Watch for changes in loggedIn and client
+
+  const toggleShowClientList = () => {
+    setShowClientList(true);
+  };
+
+  const fetchUser = async () => {
+    try {
+      const user = await requests.get.idByEmail(email);
+      setUser(user);
+      console.log(user)
+      if (user) {
+        setLoggedIn(true); // Set loggedIn to true after successful login
+        console.log("loginState", loggedIn)
+        const userData = await requests.get.userData(user.id);
+        const { clients } = userData;
+        console.log("clients", clients)
+        setClient(clients);
+        console.log("clientState", client)
+      }
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     fetchUser();
   };
-
 
   return (
     <div className="login-container">
@@ -51,6 +74,8 @@ const LoginModal = (props) => {
           </form>
         </div>
       </div>
+
+      {showClientList && <ClientList />}
     </div>
   );
 };
