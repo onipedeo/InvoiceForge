a3aa392cc2e42b465564a362a306799a64aa1956
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-11-23 15:34:50 -0500

    state and functions setup for sending data to backend

--- frontend/src/components/NewClientModal.jsx
+++ frontend/src/components/NewClientModal.jsx
@@ -1,47 +1,158 @@
-import "../styles/new-client-modal.scss"
+import { useState } from 'react';
+import "../styles/new-client-modal.scss";
+import requests from '../api/requests';
 
 export default function NewClientModal(props) {
+  const { setClientModelOpen, user } = props;
 
-  const {setClientModelOpen} = props
+  const [clientData, setClientData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    rate: 0,
+    user_id: user.id,
+  });
+
+  const [addressData, setAddressData] = useState({
+    line1: '',
+    line2: '',
+    city: '',
+    province: '',
+    country: '',
+    postalCode: '',
+  });
+
+  const handleClientInputChange = (e) => {
+   
+  };
+
+  const handleAddressInputChange = (e) => {
+  
+  };
 
   const handleClientModelClose = () => {
-    setClientModelOpen(false)
-  }
+    setClientModelOpen(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Make route requests to send the form data to the backend
+      
+
+      // Close the modal after successful submission
+      setClientModelOpen(false);
+    } catch (error) {
+      console.error('Error adding client:', error);
+     
+    }
+  };
 
   return (
-    <div className="modal" id="newClientModal">
-      <div className="modal-content">
+    <div className="new-client-modal-container" id="newClientModal">
+      <div className="new-client-modal-content">
         <span className="close" onClick={handleClientModelClose}>&times;</span>
         <h2>Add Client</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
+          {/* Client Table Data*/}
           <div className="form-group">
             <label htmlFor="name">Name:</label>
-            <input type="text" id="name" required />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={clientData.name}
+              onChange={handleClientInputChange}
+              placeholder='required'
+              required
+            />
           </div>
           <div className="form-group">
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={clientData.email}
+              onChange={handleClientInputChange}
+              placeholder='required'
+              required
+            />
           </div>
           <div className="form-group">
             <label htmlFor="phone">Phone:</label>
-            <input type="tel" id="phone" required />
+            <input
+              type="phone"
+              id="phone"
+              name="phone"
+              value={clientData.phone}
+              onChange={handleClientInputChange}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="rate">Rate:</label>
-            <input type="number" id="rate" required />
+            <input
+              type="number"
+              id="rate"
+              name="rate"
+              value={clientData.rate}
+              onChange={handleClientInputChange}
+            />
           </div>
+
+          {/* Address Table Data*/}
           <div className="address-group">
             <label>Address:</label>
-            <input type="text" placeholder="Line 1" required />
-            <input type="text" placeholder="Line 2" />
-            <input type="text" placeholder="City" required />
-            <input type="text" placeholder="Province" required />
-            <input type="text" placeholder="Country" required />
-            <input type="text" placeholder="Postal Code" required />
+            <input
+              type="text"
+              placeholder="Line 1 Required"
+              name="line1"
+              value={addressData.line1}
+              onChange={handleAddressInputChange}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Line 2"
+              name="line2"
+              value={addressData.line2}
+              onChange={handleAddressInputChange}
+            />
+            <input
+              type="text"
+              placeholder="City"
+              name="city"
+              value={addressData.city}
+              onChange={handleAddressInputChange}
+            />
+            <input
+              type="text"
+              placeholder="Province"
+              name="province"
+              value={addressData.province}
+              onChange={handleAddressInputChange}
+            />
+            <input
+              type="text"
+              placeholder="Country"
+              name="country"
+              value={addressData.country}
+              onChange={handleAddressInputChange}
+            />
+            <input
+              type="text"
+              placeholder="Postal Code"
+              name="postalCode"
+              value={addressData.postalCode}
+              onChange={handleAddressInputChange}
+            />
           </div>
+
           <button type="submit">Add Client</button>
         </form>
       </div>
     </div>
   );
 }
+
