3aee60d3dcec26dfa552d0426276c9247f6be977
carolineolagunju <olamidejr21@gmail.com>
2023-11-22 23:10:00 -0700

    styled invoice pdf

--- frontend/src/components/InvoiceGenerator.jsx
+++ frontend/src/components/InvoiceGenerator.jsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
-import requests from "../api/requests";
 
 const InvoiceGenerator = ({
   reviewedAppointments,
@@ -9,105 +8,115 @@ const InvoiceGenerator = ({
   clientRate,
   standardRateCents,
   clientObj,
-  userData
+  userObj,
 }) => {
-
-
   const [generatedPDF, setGeneratedPDF] = useState(null);
   const [errorMessage, setErrorMessage] = useState("");
 
-
-
   const generateInvoice = () => {
-
     if (checkedAppointments.length === 0) {
-      setErrorMessage("Please select at least one appointment to generate an invoice.");
+      setErrorMessage(
+        "Please select at least one appointment to generate an invoice."
+      );
       return;
     }
 
     // Create a new jsPDF instance
     const pdf = new jsPDF();
 
-    // Set font and font size
-    pdf.setFont("helvetica");
+    pdf.setLineWidth(0.5);
+    pdf.setDrawColor("black");
+    const overlineY = 25;
+    pdf.line(20, overlineY, 66, overlineY); // Draw the line
+
+    // styled text
+    pdf.setFont("sans-serif");
+    pdf.setFontSize(24);
+    pdf.setTextColor(0, 0, 0);
+
+    const text = "InvoiceForge";
+    const textX = 20;
+    const textY = 32;
+    pdf.text(textX, textY, text);
+
+    // user details
+    const userDetails = userObj.address;
     pdf.setFontSize(12);
-
-    // Add content to the PDF
-    pdf.text("Invoice", 20, 20);
-    pdf.text("LOGO", 20, 30);
-
-    // Add user and client details
-    const userDetails = userData.address;
-
+    pdf.setFont("sans-serif");
     pdf.text(
-      `User Details:
+      `
+      ${userObj.first_name} ${userObj.last_name}
       ${userDetails.line_1}
-      ${userDetails.city},
-      ${userDetails.province}
-      ${userDetails.country}
-      ${userDetails.postal_code}`,
+      ${userDetails.city}, ${userDetails.province} ${userDetails.postal_code}
+      204-444-888
+      `,
       150,
       20
     );
 
-    // Add client details
-
+    // client details
     const clientDetails = clientObj.address;
-
     pdf.text(
-      `Client Details:
+      `
+      BILL TO:
+      ${clientObj.name}
       ${clientDetails.line_1}
-      ${clientDetails.city},
-      ${clientDetails.province}
-      ${clientDetails.country}
-      ${clientDetails.postal_code}`,
+      ${clientDetails.city}, ${clientDetails.province} ${clientDetails.postal_code}
+      `,
       20,
-      65
+      55
     );
 
-
-    // Add appointment details
-    pdf.text("Appointment Details", 20, 110);
-
-    // Set styles for the table
-    pdf.setFillColor(200, 220, 255); // Header fill color
-    pdf.setTextColor(0, 0, 0); // Header text color
-    pdf.setFont("helvetica", "bold");
+    // styles for the table
+    pdf.setFillColor(200, 220, 255);
+    pdf.setTextColor(0, 0, 0);
+    pdf.setFont("sans-serif", "bold");
 
     pdf.autoTable({
-      head: [["Appointment", "Date", "Hours"]],
+      head: [["Description", "Date", "Rate per Hour", "Hours", "Total"]],
       body: reviewedAppointments
         .filter((appointment) => checkedAppointments.includes(appointment.id))
-        .map((appointment) => [
-          appointment.notes,
-          appointment.date,
-          appointment.confirmed_hours,
-        ]),
-      startY: 115,
+        .map((appointment) => {
+          const rate = appointment.appointment_rate_cents
+            ? appointment.appointment_rate_cents
+            : clientRate
+            ? clientRate
+            : standardRateCents;
+          const total = (rate * appointment.confirmed_hours) / 100;
+
+          return [
+            appointment.notes,
+            appointment.date,
+            `${appointment.appointment_rate_cents / 100}`,
+            appointment.confirmed_hours,
+            `${total}`,
+          ];
+        }),
+      startY: 90,
       theme: "striped",
     });
 
     // Reset styles
     pdf.setFillColor(255, 255, 255);
-    pdf.setFont("helvetica", "normal");
-
+    pdf.setFont("sans-serif", "bold");
+    pdf.setFontSize(14);
 
     // Calculate and add the total amount
-    const totalAmount = reviewedAppointments
+    const GrandTotalAmount = reviewedAppointments
       .filter((appointment) => checkedAppointments.includes(appointment.id))
-      .reduce(
-        (total, appointment) => {
-          const { appointment_rate_cents } = appointment;
-          const rate = appointment_rate_cents ? appointment_rate_cents :
-            clientRate ? clientRate : standardRateCents;
-          return total + appointment.confirmed_hours * rate / 100;
-        },
-        0
-      );
+      .reduce((total, appointment) => {
+        const { appointment_rate_cents } = appointment;
+        const rate = appointment_rate_cents
+          ? appointment_rate_cents
+          : clientRate
+          ? clientRate
+          : standardRateCents;
+        return total + (appointment.confirmed_hours * rate) / 100;
+      }, 0);
 
     pdf.text(
-      `Total Amount: $${totalAmount}`,
-      20,
+      `Grand Total: $${GrandTotalAmount}`,
+      150,
       pdf.autoTable.previous.finalY + 10
     );
 
@@ -134,7 +143,9 @@ const InvoiceGenerator = ({
             width="60%"
             height="500px"
           ></iframe>
-          <div><button onClick={handleConfirmAndSend}>Confirm and Send</button></div>
+          <div>
+            <button onClick={handleConfirmAndSend}>Confirm and Send</button>
+          </div>
         </div>
       )}
     </div>
