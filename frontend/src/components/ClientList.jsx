baf13290e8be53bde512dce6f6a859ee72a8bd54
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-11-22 19:59:53 -0500

    get client data, map and dispaly it

--- frontend/src/components/ClientList.jsx
+++ frontend/src/components/ClientList.jsx
@@ -1,33 +1,51 @@
-import { useState } from 'react';
-import "../styles/client-list.scss"
+import { useState, useEffect } from 'react';
+import requests from '../api/requests'; // Import your requests module
+import "../styles/client-list.scss";
 import NewClientModal from "./NewClientModal";
 
 export default function ClientList(props) {
-
   const [isClientModalOpen, setClientModelOpen] = useState(false);
-  const {user} = props
-  console.log(user)
+  const [clients, setClients] = useState([]); 
+  const { user } = props;
 
-  const fetchClient = async () => {
+  useEffect(() => {
+    fetchClients();
+  }, []); 
+
+  const fetchClients = async () => {
     try {
-      const userData = await requests.get.userData(user.id);
-      const { clients } = userData;
-      console.log("clients", clients)
+      const clientData = await requests.get.user(user.id).clients
+      console.log('Clients Data:', clientData);
+
+      setClients(clientData);
+      console.log('Clients State:', clients);
+
     } catch (error) {
       console.error('Error fetching user data:', error);
     }
   };
 
- const handleNewClientModalClick = () => {
-     setClientModelOpen(true)
+  const handleNewClientModalClick = () => {
+    setClientModelOpen(true);
   };
 
   return (
     <div className="client-list-container">
-      
-        <p>Client List Content</p>
-        <button onClick={handleNewClientModalClick}>Add New Client</button>
-        {isClientModalOpen && <NewClientModal setClientModelOpen={setClientModelOpen}/>}
+      <p>Client List Content</p>
+
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+          <span> Client's Name: {client.name}</span>
+          <span>Client's Email: {client.email}</span> 
+          <span>Client's Company: {client.company}</span>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleNewClientModalClick}>Add New Client</button>
+
+      {isClientModalOpen && <NewClientModal setClientModelOpen={setClientModelOpen} />}
     </div>
   );
-  }
+}
