e99d90915ef954e49132e92dcbc0cfc1feb99b9e
carolineolagunju <olamidejr21@gmail.com>
2023-11-22 17:18:46 -0700

    removed unused variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,15 +9,27 @@ import ClientList from "./components/ClientList";
 function App() {
   const handleLinkClick = (pageNumber) => {
     setDisplayPage(pageNumber);
-  }
+  };
+
   const [user, setUser] = useState(null);
+  const [displayPage, setDisplayPage] = useState(0);
+
 
   return (
     <>
-      <TopNavBar user={user} setUser={setUser} handleLinkClick={handleLinkClick}/>
-      {displayPage=== 0 && <LandingPage />}
-      {displayPage=== 2 && <ClientList user={user}/>}
-      {displayPage === 4 && <AppointmentContainer userId={user.id} standardRateCents={user.standard_rate_cents}/>}
+      <TopNavBar
+        user={user}
+        setUser={setUser}
+        handleLinkClick={handleLinkClick}
+      />
+      {displayPage === 0 && <LandingPage />}
+      {displayPage === 2 && <ClientList />}
+      {displayPage === 4 && (
+        <AppointmentContainer
+          userId={user.id}
+          standardRateCents={user.standard_rate_cents}
+        />
+      )}
       <Footer />
       {/* <AppointmentContainer userId={user.id} standardRateCents={user.standard_rate_cents}/> */}
     </>
