967fece814ecb633b699760732348d560d8fdd2b
Tobi Onipede <onipede89@gmail.com>
2023-11-24 22:44:36 +0100

    add getting the day of appointment function from the database

--- frontend/src/components/Day.jsx
+++ frontend/src/components/Day.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import '../styles/day.scss';
 import Appointments from './Appointments';
+import requests from "../api/requests";
 
 
-const day = ["Sunday", "Monday", "Tuesday", "wednessday", "Thursday", "Friday", "Saturday"];
+const day = ["Sunday", "Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday"];
 
+
+const dayString = () => {
+  requests.get.user(1).appointments.then((appointments) => {
+    const dayFromDb = appointments.map((data) => {
+      const dateObject = new Date(data.date);
+      const dayOfTheWeek = dateObject.getDay();
+      const day = ["Sunday", "Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday"];
+      const dayName = day[dayOfTheWeek]
+      console.log(dayName);
+      
+    })
+  })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+};
+
+
+dayString();
 
 const dayList = day.map((day, index) => {
 
