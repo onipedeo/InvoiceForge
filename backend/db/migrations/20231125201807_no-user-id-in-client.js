0af9f44ba19bb1f61d545b57344ae6d2283eb54d
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-25 15:44:32 -0500

    change user client to many-many

---
+++ backend/db/migrations/20231125201807_no-user-id-in-client.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('clients_users', function(table) {
+      table.integer('user_id').references('id').inTable('users').notNullable();
+      table.integer('client_id').references('id').inTable('clients').notNullable();
+
+    })
+    .table('clients', function(table) {
+      table.dropColumn('user_id');
+    });
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema
+    .table('clients', function(table) {
+      table.integer('user_id').references('id').inTable('users').notNullable().defaultTo(1);
+    })
+    .dropTable('clients_users');
+};
