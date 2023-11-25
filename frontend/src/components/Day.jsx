23f329a2850cd42aeba56b070e14c4b75ac845ff
Tobi Onipede <onipede89@gmail.com>
2023-11-25 05:43:44 +0100

    add call request from frontend and display on schedule page

--- frontend/src/components/Day.jsx
+++ frontend/src/components/Day.jsx
@@ -64,38 +64,54 @@ const Day = () => {
 =======
 import React from 'react';
 import '../styles/day.scss';
-import Appointments from './Appointments';
 import requests from "../api/requests";
+import { Calendar, momentLocalizer, DateLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import "react-big-calendar/lib/css/react-big-calendar.css"
 
 
-const day = ["Sunday", "Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday"];
+const localizer = momentLocalizer(moment);
 
 
-const dayString = () => {
-  requests.get.user(1).appointments.then((appointments) => {
-    const dayFromDb = appointments.map((data) => {
-      const dateObject = new Date(data.date);
-      const dayOfTheWeek = dateObject.getDay();
-      const day = ["Sunday", "Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday"];
-      const dayName = day[dayOfTheWeek]
-      console.log(dayName);
-      
-    })
-  })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
-};
+const Day = () => {
 
+  const [events, setevents] = useState([]);
 
-dayString();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const appointments = await requests.get.user(1).appointments;
+        const sortedEvents = [];
 
-const dayList = day.map((day, index) => {
+        const appointmentEvents = appointments.map((app) => {
+
+          const evt = {
+            start: moment(`${app.date}T${app.startTime}`).toDate(),
+            end: moment(`${app.date}T${app.endTime}`).toDate(),
+            title: `${app.notes}`
+          }
+          sortedEvents.push(evt);
+
+        })
+        setevents(sortedEvents)
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [])
 
   return (
-    <div key={index} className='day-housing'>
-      <div>
-        {day}
+    <>
+      <div className="myCustomHeight" style={{ height: "80vh" }}>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+        />
       </div>
       <Appointments />
     </div>
