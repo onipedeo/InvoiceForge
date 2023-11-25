df7959d10ba70811ae984584ccd79adb0d8c5f3e
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-11-25 02:13:59 -0500

    establish a puts route in the clientRoutes file with path '/:id/delete'

---
+++ backend/db/migrations/20231125064548_migration_name.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('clients', function(table) {
+    table.boolean('deleted').defaultTo(false);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.table('clients', function(table) {
+    table.dropColumn('deleted');
+  });
+  
+};
