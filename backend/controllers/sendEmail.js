cae7385cada23c548847cabbe6bc3e43a1cd5fa9
carolineolagunju <olamidejr21@gmail.com>
2023-11-25 17:59:49 -0700

    delete debugging console

--- backend/controllers/sendEmail.js
+++ backend/controllers/sendEmail.js
@@ -4,8 +4,6 @@ class SendEmailController {
   async invoice(req, res) {
     try {
       sendInvoice({pdfAttachment: req.file, ...req.body});
-      console.log("reqbody", req.body);
-      console.log("req.file", req.file);
       
     } catch (e) {
       res.status(500).json({ error: 'Internal server error' });
