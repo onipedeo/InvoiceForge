46dbc09fcc5fcfb4e2645382bdb0821cef4b894e
Tobi Onipede <onipede89@gmail.com>
2023-11-27 00:31:45 +0100

    WIP addEditModal

--- frontend/src/components/Page.jsx
+++ frontend/src/components/Page.jsx
@@ -1,6 +1,5 @@
 import { React, useState } from 'react';
 import '../styles/page.scss';
-import Schedule from './Schedule';
 import AddEditModal from './AddEditModal';
 import Day from './Day';
 
@@ -8,6 +7,11 @@ import Day from './Day';
 const Page = (props) => {
 
   const [isModalOpen, setModalOpen] = useState(false);
+  const [formData, setFormData] = useState(
+    {
+
+    }
+  )
 
   const openModal = () => {
     setModalOpen(true);
