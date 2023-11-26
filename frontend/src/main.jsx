b7d0643305cb804b381ee16114d9892ea5dc5c83
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-26 18:22:45 -0500

    import syle aids

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
