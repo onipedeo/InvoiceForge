288e09247358706943feb56f838ceb2f3d4ab90f
carolineolagunju <olamidejr21@gmail.com>
2023-11-25 17:35:30 -0700

    removed unused imports

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
-import Commonjs from 'vite-plugin-commonjs';
 
 dotenv.config({ path: process.cwd() + '/.env' });
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [react()],
-  plugins: [react(), Commonjs()],
+  plugins: [react()],
   server: {
     port: process.env.PORT,
     host: process.env.HOST,
