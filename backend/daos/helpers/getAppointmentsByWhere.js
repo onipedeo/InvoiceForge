b20a9b2579ec1c7b5da4f207d3d9b26e5e1ac5f0
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-25 21:12:34 -0500

    adjust helpers to fit

--- backend/daos/helpers/getAppointmentsByWhere.js
+++ backend/daos/helpers/getAppointmentsByWhere.js
@@ -3,7 +3,21 @@ const humps = require('humps');
 
 
 module.exports = async function (where) {
+  if (!!where.user_id) {
+    const {user_id, ...where} = where;
+    const clients = await db('clients_users').where({ user_id });
+   // generate array of unique client ids
+   const clientIds = clients.map(client => client.client_id);
+   const uniqueClientIds = [...new Set(clientIds)];
 
-  const invoices = await db('appointments').where(where);
-  return humps.camelizeKeys(invoices);
+   // retrieve all invoices for each client
+   const appointments = await Promise.all(uniqueClientIds.map(async (clientId) => {
+     return await db('appointments').where({ client_id: clientId });
+   }));
+
+  return humps.camelizeKeys(appointments);
+  } else {
+    const appointments = await db('appointments').where(where);
+    return humps.camelizeKeys(appointments);
+  }
 };
