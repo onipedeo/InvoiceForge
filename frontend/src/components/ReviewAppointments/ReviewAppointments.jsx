6c1af4359ef677faee74588250cde0814ec72f7f
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-27 08:45:40 -0500

    Build ReviewAppointments

--- frontend/src/components/ReviewAppointments/ReviewAppointments.jsx
+++ frontend/src/components/ReviewAppointments/ReviewAppointments.jsx
@@ -1,34 +1,39 @@
-import { useContext} from 'react';
-
+import { useContext, useEffect } from 'react';
 import AppointmentList from './AppointmentList/AppointmentList';
-// import ReviewModal from './ReviewModal';
-import { ReviewAppointmentsContext } from './UseReviewAppointmentsContext';
+import { ReviewAppointmentsContext } from './Context/UseReviewAppointmentsContext';
+import ConfirmationWizard from './Modals/confirmationWizard/confirmationWizard';
+import AlertModal from './Modals/AlertModal';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './ReviewAppointments.scss';
 
 const ReviewAppointments = ({ setDisplayPage }) => {
   const { state, dispatch, actions } = useContext(ReviewAppointmentsContext);
-  const { unreviewed, reviewed, modalIsOpen, modalAppointment } = state;
+  const { unreviewed, reviewed, modalIsOpen, modalAppointment, isError, errMessage} = state;
 
-  return (
-    <section className='lists-container'>
-
-      <article className='appointment-list unreviewed'>
-        <h2>Confirm Your Hours</h2>
-        <AppointmentList key={1} appointments={unreviewed} />
-      </article>
-
-
-
-      <article className='appointment-list reviewed'>
-        <h2>Confirmed and Pending </h2>
-        <AppointmentList key={2} appointments={reviewed} />
-      </article>
-
-
-      {modalIsOpen && <ReviewModal closeModal={closeModal} addToReviewed={addToReviewed} modalAppointment={modalAppointment} />}
-
-    </section>
-
-  );
+  useEffect(() => {
+    dispatch({ type: actions.setIsLoading, payload: { isLoading: true } });
+  }, []);
+    return(
+      <section className='container'>
+        <div className='col'>
+          <article className='row card'>
+            <h2>Confirm Your Hours</h2>
+            <div className="table-container">
+              <AppointmentList key={1} mode={'unreviewed'} />
+            </div>
+          </article>
+          <article className='row card'>
+            <h2>Confirmed and Pending </h2>
+            <div className="table-container">
+              <AppointmentList key={2} mode={'reviewed'} />
+            </div>
+          </article>
+        </div>
+        {modalIsOpen && <ReviewModal modalAppointment={modalAppointment} />}
+        {isError && <AlertModal errMessage={errMessage} />}
+      </section>
+    );
 };
 
 export default ReviewAppointments;
