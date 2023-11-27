8fc9435c3690bb33a18773f551b88d6abac0f1e7
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-27 08:33:48 -0500

    import bootstrap to main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'font-awesome/css/font-awesome.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
