0a483d06ba3456f4434506010ddd6f6c5ea478b6
Tobi Onipede <onipede89@gmail.com>
2023-11-27 20:28:10 +0100

    Add Create appointment operation to the db successfully

--- frontend/src/components/AddEditModal.jsx
+++ frontend/src/components/AddEditModal.jsx
@@ -1,109 +1,141 @@
-import React, { useEffect, useState } from 'react';
-import '../styles/addEditModal.scss';
-import requests from '../api/requests';
+import React, { useEffect, useState } from "react";
+import "../styles/addEditModal.scss";
+import requests from "../api/requests";
 
 const AddEditModal = (props) => {
-  const [formData, setFormData] = useState({});
+	const [formData, setFormData] = useState({
+		appointmentRateCents: "",
+		date: "",
+		endTime: "",
+		notes: "",
+		startTime: "",
+		clientId: "",
+	});
+	const [clients, setClients] = useState([]);
 
-  const handleChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
+	const handleChange = (event) => {
+		const type = event.target.type;
+		const name = event.target.name;
 
+		const value =
+			type === "checkbox" ? event.target.checked : event.target.value;
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formData);
-    try {
-      const response = await requests.create.appointment(formData);
+		if (event.target.tagName.toLowerCase() === "select") {
+			const selectedClient = clients.find(
+				(client) => client.id === parseInt(event.target.value)
+			);
+			setFormData((formData) => ({
+				...formData,
+				clientId: selectedClient ? selectedClient.id : "", // Update clientId
+			}));
+		} else {
+			setFormData((formData) => ({
+				...formData,
+				[name]: value,
+			}));
+		}
+	};
 
-      if (!response.ok) {
-        throw new Error(`Failed to send data: ${response.statusText}`);
-      }
+	useEffect(() => {
+		requests.get.user(props.user.id).clients.then((clients) => {
+			setClients(clients);
+			console.log("clients", clients);
+		});
+	}, [props.user]);
 
-      // Handle the response
-      const responseData = await response.json();
-      console.log('Data sent successfully', responseData);
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		console.log(formData);
+		try {
+			const response = await requests.create.appointment(formData);
+			console.log("response", response);
 
-      // Close the modal
-      props.onClose();
-    } catch (error) {
-      console.error('Error sending data', error);
-    }
-  };
+			// Close the modal
+			props.onClose();
+		} catch (error) {
+			console.error("Error sending data", error);
+		}
+	};
 
+	return (
+		props.isOpen && (
+			<div className="addEditModal">
+				<h2>Book an Appointment</h2>
 
+				<form>
+					<button
+						className="close-modal"
+						type=""
+						onClick={props.onClose}
+					>
+						❌
+					</button>
+					<label>Client Name:</label>
+					<select
+						name="clientName"
+						id=""
+						value={formData.clientName}
+						onChange={handleChange}
+						type="text"
+					>
+						{clients.map((client) => (
+							<option
+								key={client.id}
+								name="clientId"
+								value={client.id}
+							>
+								{client.name}
+							</option>
+						))}
+					</select>
 
-
-  return (
-    props.isOpen && (
-
-      <div className='addEditModal'>
-        <h2>Book an Appointment</h2>
-
-        <form >
-          <button className="close-modal" type='submit' onClick={props.onClose}>❌</button>
-          <label htmlFor="Title">
-            Client Name:
-          </label>
-          <select name="clientName" id="">
-            <option value=""></option>
-          </select>
-          <input
-            type="text"
-            name="clientName"
-            value={formData.clientName}
-            onChange={handleChange}
-          />
-          <label>
-            Date:
-          </label>
-          <input
-            type="date"
-            name="date"
-            value={formData.date}
-            onChange={handleChange}
-          />
-          <label>
-            Start Time:
-          </label>
-          <input
-            type="time"
-            name="startTime"
-            value={formData.startTime}
-            onChange={handleChange}
-          />
-          <label>
-            End Time:
-          </label>
-          <input
-            type="time"
-            name="endTime"
-            value={formData.endTime}
-            onChange={handleChange}
-          />
-          <label>
-            Appointment Rate (cents) :
-          </label>
-          <input type="number" step={100} name='appointment_rate_cents' value={formData.appointment_rate_cents}
-            onChange={handleChange} />
-          <label>
-            Notes:
-          </label>
-          <textarea
-            name="notes"
-            value={formData.notes}
-            onChange={handleChange}
-          />
-          <br />
-          <button className='addEditModalBtn' type="submit" onClick={handleSubmit}>Save Appointment</button>
-        </form>
-
-      </div>
-    )
-  );
+					<label>Date:</label>
+					<input
+						type="date"
+						name="date"
+						value={formData.date}
+						onChange={handleChange}
+					/>
+					<label>Start Time:</label>
+					<input
+						type="time"
+						name="startTime"
+						value={formData.startTime}
+						onChange={handleChange}
+					/>
+					<label>End Time:</label>
+					<input
+						type="time"
+						name="endTime"
+						value={formData.endTime}
+						onChange={handleChange}
+					/>
+					<label>Appointment Rate (cents) :</label>
+					<input
+						type="number"
+						step={100}
+						name="appointmentRateCents"
+						value={formData.appointment_rate_cents}
+						onChange={handleChange}
+					/>
+					<label>Notes:</label>
+					<textarea
+						name="notes"
+						value={formData.notes}
+						onChange={handleChange}
+					/>
+					<br />
+					<button
+						className="addEditModalBtn"
+						type="submit"
+						onClick={handleSubmit}
+					>
+						Save Appointment
+					</button>
+				</form>
+			</div>
+		)
+	);
 };
 
 export default AddEditModal;
--- frontend/src/components/Day.jsx
+++ frontend/src/components/Day.jsx
@@ -2,52 +2,52 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/day.scss';
 import requests from "../api/requests";
-import { Calendar, momentLocalizer, DateLocalizer } from 'react-big-calendar'
-import moment from 'moment'
-import "react-big-calendar/lib/css/react-big-calendar.css"
-
+import { Calendar, momentLocalizer, DateLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 
+const Day = (props) => {
+	const [events, setevents] = useState([]);
+	const [clientData, setClientData] = useState([]);
 
-const Day = () => {
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const appointments = await requests.get.user(props.user.id)
+					.appointments;
+				const sortedEvents = [];
+				const clients = await requests.get.user(props.user.id).clients;
 
-  const [events, setevents] = useState([]);
-  const [clientData, setClientData] = useState([]);
+				// console.log('clients', clients);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const appointments = await requests.get.user(1).appointments;
-        const sortedEvents = [];
-        const clients = await requests.get.user(1).clients;
+				const appointmentEvents = appointments.map((app) => {
+					const evt = {
+						start: moment(`${app.date}T${app.startTime}`).toDate(),
+						end: moment(`${app.date}T${app.endTime}`).toDate(),
+						title: `${app.notes}`,
+					};
+					sortedEvents.push(evt);
+				});
+				setevents(sortedEvents);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		};
 
-        console.log('clients', clients);
+		fetchData();
+	}, []);
 
-        const appointmentEvents = appointments.map((app) => {
+	//function to handle edit
+	const handleSelectEvent = () => {
+		alert("event clicked");
+	};
 
-          const evt = {
-            start: moment(`${app.date}T${app.startTime}`).toDate(),
-            end: moment(`${app.date}T${app.endTime}`).toDate(),
-            title: `${app.notes}`
-          }
-          sortedEvents.push(evt);
-
-        })
-        setevents(sortedEvents)
-
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, [])
-
-  const minTime = new Date();
-  minTime.setHours(5, 30, 0);
-  const maxTime = new Date();
-  maxTime.setHours(20, 30, 0);
+	const minTime = new Date();
+	minTime.setHours(5, 30, 0);
+	const maxTime = new Date();
+	maxTime.setHours(20, 30, 0);
 
   return (
     <div className="myCustomHeight" style={{ height: "80vh" }}>
