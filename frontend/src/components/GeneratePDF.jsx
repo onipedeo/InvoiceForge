23fa75ef0b10eb95e1ebbce6c8596d60e497691f
carolineolagunju <olamidejr21@gmail.com>
2023-11-26 21:59:27 -0700

    create component GeneratePDF

---
+++ frontend/src/components/GeneratePDF.jsx
@@ -0,0 +1,149 @@
+
+import React from 'react';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+
+const GeneratePDF = ({
+  reviewedAppointments,
+  checkedAppointments,
+  clientRate,
+  clientObj,
+  user,
+  setGeneratedPDF,
+  setGeneratedAttachment,
+  setErrorMessage,
+}) => {
+
+  const generateInvoice = () => {
+    if (checkedAppointments.length === 0) {
+      setErrorMessage(
+        "Please select at least one appointment to generate an invoice."
+      );
+      return;
+    }
+
+    // Create a new jsPDF instance
+    const pdf = new jsPDF();
+
+    pdf.setLineWidth(0.5);
+    pdf.setDrawColor("black");
+    const overlineY = 25;
+    pdf.line(20, overlineY, 66, overlineY); // Draw the line
+
+    // styled text
+    pdf.setFont("sans-serif");
+    pdf.setFontSize(24);
+    pdf.setTextColor(0, 0, 0);
+
+    const text = "InvoiceForge";
+    const textX = 20;
+    const textY = 32;
+    pdf.text(textX, textY, text);
+
+    // user details
+    const userDetails = user.address;
+    pdf.setFontSize(12);
+    pdf.setFont("sans-serif");
+    pdf.text(
+      `
+      ${user.firstName} ${user.lastName}
+      ${userDetails.line1}
+      ${userDetails.city}, ${userDetails.province} ${userDetails.postalCode}
+      204-444-888
+      `,
+      150,
+      20
+    );
+
+    // client details
+    let clientDetails = clientObj.address;
+    if (!clientDetails) {
+      clientDetails = {
+        line1: "",
+        city: "",
+        province: "",
+        postalCode: "",
+      };
+    }
+    pdf.text(
+      `
+      BILL TO:
+      ${clientObj.name}
+      ${clientDetails.line1}
+      ${clientDetails.city} ${clientDetails.city ? "," : ""} ${
+        clientDetails.province
+      } ${clientDetails.postalCode}
+      `,
+      20,
+      55
+    );
+
+    // styles for the table
+    pdf.setFillColor(200, 220, 255);
+    pdf.setTextColor(0, 0, 0);
+    pdf.setFont("sans-serif", "bold");
+
+    pdf.autoTable({
+      head: [["Description", "Date", "Rate ($)", "Hours", "Total ($)"]],
+
+      body: reviewedAppointments
+        .filter((appointment) => checkedAppointments.includes(appointment.id))
+        .map((appointment) => {
+          const rate = appointment.appointmentRateCents
+            ? appointment.appointmentRateCents
+            : clientRate
+            ? clientRate
+            : user.standardRateCents;
+          const total = (rate * appointment.confirmedHours) / 100;
+
+          return [
+            appointment.notes,
+            appointment.date,
+            `${rate / 100}`,
+
+            appointment.confirmedHours,
+            `${total}`,
+          ];
+        }),
+      startY: 90,
+      theme: "striped",
+    });
+
+    // Reset styles
+    pdf.setFillColor(255, 255, 255);
+    pdf.setFont("sans-serif", "bold");
+    pdf.setFontSize(14);
+
+    // Calculate and add the total amount
+    const GrandTotalAmount = reviewedAppointments
+      .filter((appointment) => checkedAppointments.includes(appointment.id))
+      .reduce((total, appointment) => {
+        const { appointmentRateCents } = appointment;
+        const rate = appointmentRateCents
+          ? appointmentRateCents
+          : clientRate
+          ? clientRate
+          : user.standardRateCents;
+        return total + (appointment.confirmedHours * rate) / 100;
+      }, 0);
+
+    pdf.text(
+      `Grand Total: $ ${GrandTotalAmount}.00`,
+      140,
+      pdf.autoTable.previous.finalY + 15
+    );
+
+    // Save the PDF
+    const generatedPDFData = pdf.output("blob");
+    setGeneratedPDF(URL.createObjectURL(generatedPDFData));
+    setGeneratedAttachment(
+      new File([generatedPDFData], "invoice.pdf", { type: "application/pdf" })
+    );
+
+    setErrorMessage("");
+  };
+
+  return <div onClick={generateInvoice}>Generate Invoice</div>;
+};
+
+export default GeneratePDF;
