c97ed58408876ab748f2b62870b85be33dd19395
carolineolagunju <olamidejr21@gmail.com>
2023-11-26 21:59:59 -0700

    refactor code

---
+++ frontend/src/components/InvoiceSentModal.jsx
@@ -0,0 +1,15 @@
+import "../styles/InvoiceGeneratedModal.scss"
+
+const InvoiceSentModal = (props) => {
+
+  return (
+    <div className="invoice-generated-modal">
+        <p>INVOICE SENT!!!</p>
+      <div className="modal-options">
+        <button onClick={props.onClick}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default InvoiceSentModal;
