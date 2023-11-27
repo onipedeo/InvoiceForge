051e1bd79f9f44ed792147220bb6b11aa9aa3ffd
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-27 08:40:03 -0500

    build appointment component for review

--- frontend/src/components/ReviewAppointments/AppointmentList/Appointment/Appointment.jsx
+++ frontend/src/components/ReviewAppointments/AppointmentList/Appointment/Appointment.jsx
@@ -1,52 +1,48 @@
 import { useState, useContext, useEffect } from 'react';
-import { ReviewAppointmentsContext, actions } from '../../UseReviewAppointmentsContext';
+import { ReviewAppointmentsContext, actions } from '../../Context/UseReviewAppointmentsContext';
 import moment from 'moment';
 
 const Appointment = ({ appointment }) => {
   const { state, dispatch } = useContext(ReviewAppointmentsContext);
-
   const [hours, setHours] = useState(0);
   const increment = () => setHours(prevValue => ++prevValue);
   const decrement = () => setHours(prevValue => --prevValue);
-  let confirmedHours;
-  let date;
-  let clientName;
-  let notes;
-  let id;
 
-
+  // Extract values from the appointment prop
+  const { confirmedHours, date, client, client: { id: client_id }, startTime, endTime, id } = appointment;
 
   useEffect(() => {
-    if (typeof appointment === 'object') {
+    if (typeof appointment === 'object' && Object.keys(appointment).length > 0) {
       if (appointment.confirmedHours) {
         setHours(appointment.confirmedHours);
-
-      }
-      else {
+      } else {
         const { startTime, endTime } = appointment;
-        const estimatedHours = moment(endTime).diff(moment(startTime), 'hours');
+        const estimatedHours = moment.duration(moment(endTime, "HH:mm:ss").diff(moment(startTime, "HH:mm:ss"))).asHours();
         const roundedEstimatedHours = Math.round(estimatedHours / 0.25) * 0.25;
         setHours(roundedEstimatedHours);
       }
     }
   }, [appointment]);
+
   return (
     <tr key={id} className="appointment-list-item">
-      <td>{clientName}</td>
-      <td>{notes}</td>
+      <td>{client.name}</td>
       <td>{date}</td>
-      <td>{
+      <td>{startTime}</td>
+      <td>{endTime}</td>
+      <td className='col-auto'>{
         confirmedHours ||
-        <input type="number" value={hours} onChange={(e) => {
+        <input type="number" className='number-input' name="confirmedHours" min="0.25" step="0.25" value={hours} onChange={(e) => {
           setHours(e.target.value);
         }} />
-
       }</td>
       <td>
         <button
           onClick={
             () => {
-              dispatch({ type: actions.moveToReviewed, payload: appointment });
+              dispatch({ type: actions.setIsLoading, payload: true });
+              dispatch({ type: actions.moveToReviewed, payload: { formData } });
+
             }
           }
         >
@@ -56,6 +52,5 @@ const Appointment = ({ appointment }) => {
     </tr>
   );
 };
-
 
 export default Appointment;
