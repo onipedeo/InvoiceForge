275260419c9655c43917438c7ed9538ff4168d54
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-28 01:52:20 -0500

    complete the refactor to helper

--- frontend/src/components/ReviewAppointments/AppointmentList/AppointmentList.jsx
+++ frontend/src/components/ReviewAppointments/AppointmentList/AppointmentList.jsx
@@ -21,45 +21,7 @@ const AppointmentList = ({ mode }) => {
   }, [appointments, dispatch]);
 
   // function to render the appointments
-  const componentAppointments = () => {
-    // check if appointments is null or undefined.
-    if (appointments !== null && appointments !== undefined && appointments.length > 0) {
-      // if not, then map the appointments to the Appointment component
-      return appointments.map((appointment) => (
-        <Appointment
-          key={appointment.id}
-          appointment={appointment}
-        />
-      ));
-      // if empty array, display a message
-    }
-    if (Array.isArray(appointments) && appointments.length === 0) {
-      return (
-        <tr>
-          <td className="text-center align-middle" colSpan="5">
-            <h3>You have no {mode} appointments.</h3>
-            <p> What next?</p>
-            <span className="btn btn-primary">Generate Invoice</span>
-            <p>Or</p>
-            <span className="btn btn-primary">Schedule more appointments</span>
-
-
-          </td>
-        </tr>
-      );
-    }
-    // if appointments is null or undefined, then display a spinner
-    return (
-      <tr>
-        {state.isLoading && (
-          <div className="spinner-border text-primary" role="status">
-            <span className="sr-only">Loading...</span>
-          </div>
-        )}
-        <td colSpan="5">Loading...</td>
-      </tr>
-    );
-  };
+  const componentAppointments = renderAppointments(mode);
 
   return (
     <>
@@ -74,7 +36,7 @@ const AppointmentList = ({ mode }) => {
           </tr>
         </thead>
         <tbody>
-          {componentAppointments()}
+          {componentAppointments}
         </tbody>
       </table>
       {state.isLoading && (
