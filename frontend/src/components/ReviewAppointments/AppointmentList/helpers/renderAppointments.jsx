30457e57e89b5b0a20c423594d549ee0f6211c62
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-28 02:59:55 -0500

    tidy up

--- frontend/src/components/ReviewAppointments/AppointmentList/helpers/renderAppointments.jsx
+++ frontend/src/components/ReviewAppointments/AppointmentList/helpers/renderAppointments.jsx
@@ -2,32 +2,43 @@ import { ReviewAppointmentsContext } from "../../Context/UseReviewAppointmentsCo
 import { useContext } from "react";
 import Appointment from "../Appointment/Appointment";
 
-export default (mode) => {
+export default (mode, setDisplayPage) => {
   const { state, dispatch, actions } = useContext(ReviewAppointmentsContext);
   //get the appointments from the context based on the mode
   const appointments = mode === 'unreviewed' ? state.unreviewed : state.reviewed;
 
 
-  // check if appointments is null or undefined.
+  // Make sure the appointments are not null or undefined
   if (appointments !== null && appointments !== undefined && appointments.length > 0) {
-    // if not, then map the appointments to the Appointment component
+    //map the appointments to the Appointment component
     return appointments.map((appointment) => (
       <Appointment
         key={appointment.id}
         appointment={appointment}
       />
     ));
-    // if empty array, display a message
   }
+
+  // if empty array, display a message
   if (Array.isArray(appointments) && appointments.length === 0) {
     return (
       <tr>
         <td className="text-center align-middle" colSpan="5">
           <h3>You have no {mode} appointments.</h3>
           <p> What next?</p>
-          <span className="btn btn-primary">Generate Invoice</span>
+          <span
+            onClick={() => setDisplayPage(4)}
+            className="btn btn-primary"
+          >
+            Generate Invoice
+          </span>
           <p>Or</p>
-          <span className="btn btn-primary">Schedule more appointments</span>
+          <span
+            onClick={() => setDisplayPage(0)}
+            className="btn btn-primary"
+          >
+            Schedule more appointments
+          </span>
 
         </td>
       </tr>
@@ -38,12 +49,12 @@ export default (mode) => {
     return (
       <tr>
         <td colSpan="5">
-          <div className="spinner-border text-primary" role="status">
-            <span className="sr-only">Loading...</span>
+          <div className="spinner-border text-primary justify-content-center" role="status">
+            <span className="sr-only"></span>
           </div>
         </td>
       </tr>
     );
   }
-  
+
 };
