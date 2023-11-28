61b3bc9742a178329462ef7880d29aa50e8711f8
carolineolagunju <olamidejr21@gmail.com>
2023-11-28 16:11:43 -0700

    post request to create invoice in the db

--- frontend/src/components/InvoiceGenerator.jsx
+++ frontend/src/components/InvoiceGenerator.jsx
@@ -12,29 +12,61 @@ const InvoiceGenerator = ({
   const [generatedPDF, setGeneratedPDF] = useState(null);
   const [errorMessage, setErrorMessage] = useState("");
   const [generatedAttachment, setGeneratedAttachment] = useState(null);
-  const [isModalOpen, setModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [grandTotal, setGrandTotal] = useState(0);
 
-  const handleConfirmAndSend = () => {
-    const email = clientObj.email;
-    const userName = user.firstName;
-    const clientName = clientObj.name;
+  const handleConfirmAndSend = async () => {
+    try {
+      const email = "clientObj.email";
+      const userName = user.firstName;
+      const clientName = clientObj.name;
 
-    const formData = new FormData();
-    formData.append("email", email);
-    formData.append("userName", userName);
-    formData.append("clientName", clientName);
-    formData.append("pdf", generatedAttachment);
+      const formData = new FormData();
+      formData.append("email", email);
+      formData.append("userName", userName);
+      formData.append("clientName", clientName);
+      formData.append("pdf", generatedAttachment);
 
-    const requestOptions = {
-      method: "POST",
-      body: formData,
-    };
-    fetch("/api/send_email/invoice", requestOptions);
-    setModalOpen(true);
+      const requestOptions = {
+        method: "POST",
+        body: formData,
+      };
+
+      const invoiceData = {
+        userId: user.id,
+        clientId: clientObj.id,
+        totalCents: grandTotal,
+        appointmentIds: checkedAppointments,
+      };
+
+      const invoiceDataPostRequest = {
+        method: "POST",
+        body: JSON.stringify(invoiceData),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      };
+
+      // Wait for the invoice creation request to complete
+      await fetch("/api/invoice", invoiceDataPostRequest);
+
+      setIsModalOpen(true);
+      // Wait for the email sending request to complete
+      await fetch("/api/send_email/invoice", requestOptions);
+
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   const handleCloseModal = () => {
-    setModalOpen(false);
+    setIsModalOpen(false);
+    setGeneratedPDF(null);
+  };
+
+  const handleBackToAppointments = () => {
+    setGeneratedPDF(null);
   };
 
   return (
@@ -48,10 +80,11 @@ const InvoiceGenerator = ({
         setGeneratedPDF={setGeneratedPDF}
         setGeneratedAttachment={setGeneratedAttachment}
         setErrorMessage={setErrorMessage}
+        setGrandTotal={setGrandTotal}
       />
       {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       {generatedPDF && (
-        <div className="pdf-modal">
+        <div className="pdf">
           <iframe
             title="Invoice PDF"
             src={generatedPDF}
@@ -59,6 +92,9 @@ const InvoiceGenerator = ({
             height="500px"
           ></iframe>
           <div>
+            <button onClick={handleBackToAppointments}>
+              Back to Appointments
+            </button>
             <button onClick={handleConfirmAndSend}>Confirm and Send</button>
           </div>
         </div>
