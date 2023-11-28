d229f8ca2801d51ceb8c563f3f7e0873cfc365df
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-27 23:15:50 -0500

    rework Appointment

--- frontend/src/components/ReviewAppointments/AppointmentList/Appointment/Appointment.jsx
+++ frontend/src/components/ReviewAppointments/AppointmentList/Appointment/Appointment.jsx
@@ -1,6 +1,9 @@
 import { useState, useContext, useEffect } from 'react';
 import { ReviewAppointmentsContext, actions } from '../../Context/UseReviewAppointmentsContext';
 import moment from 'moment';
+import './Appointment.scss';
+import { FaCheck } from 'react-icons/fa';
+import requests from '../../../../api/requests';
 
 const Appointment = ({ appointment }) => {
   const { state, dispatch } = useContext(ReviewAppointmentsContext);
@@ -9,13 +12,15 @@ const Appointment = ({ appointment }) => {
   const decrement = () => setHours(prevValue => --prevValue);
 
   // Extract values from the appointment prop
-  const { confirmedHours, date, client, client: { id: client_id }, startTime, endTime, id } = appointment;
+  const { confirmedHours, date, client, notes, startTime, endTime, id } = appointment;
+  const [refresh, setRefresh] = useState(false);
+  useEffect(() => {
+    if (refresh) setRefresh(false);
+  }, [refresh]);
 
   useEffect(() => {
     if (typeof appointment === 'object' && Object.keys(appointment).length > 0) {
-      if (appointment.confirmedHours) {
-        setHours(appointment.confirmedHours);
-      } else {
+      if (confirmedHours) {
         const { startTime, endTime } = appointment;
         const estimatedHours = moment.duration(moment(endTime, "HH:mm:ss").diff(moment(startTime, "HH:mm:ss"))).asHours();
         const roundedEstimatedHours = Math.round(estimatedHours / 0.25) * 0.25;
@@ -24,12 +29,29 @@ const Appointment = ({ appointment }) => {
     }
   }, [appointment]);
 
+  const handleUpdate = () => {
+    dispatch({ type: actions.setIsLoading, payload: true });
+    const formObject = {
+      ...appointment,
+      confirmedHours: hours,
+      clientId: appointment.client.id,
+    };
+
+    requests.update.appointment(id, formObject).then(() => {
+      dispatch({ type: actions.moveToReviewed, payload: formObject });
+      setRefresh(true);
+    }).catch((e) => {
+      console.log(e);
+      dispatch({ type: actions.setErrMessage, payload: "Sorry, there was an issue updating your appointment" });
+      dispatch({ type: actions.openAlert });
+    });
+  };
+
   return (
     <tr key={id} className="appointment-list-item">
       <td>{client.name}</td>
       <td>{date}</td>
-      <td>{startTime}</td>
-      <td>{endTime}</td>
+      <td>{notes}</td>
       <td className='col-auto'>{
         confirmedHours ||
         <input type="number" className='number-input' name="confirmedHours" min="0.25" step="0.25" value={hours} onChange={(e) => {
@@ -38,15 +60,14 @@ const Appointment = ({ appointment }) => {
       }</td>
       <td>
         <button
+          className="btn check-btn"
           onClick={
             () => {
-              dispatch({ type: actions.setIsLoading, payload: true });
-              dispatch({ type: actions.moveToReviewed, payload: { formData } });
-
+              handleUpdate();
             }
           }
         >
-          confirm
+          <FaCheck />
         </button>
       </td>
     </tr>
