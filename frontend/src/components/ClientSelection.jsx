d69c1b140a5c02112c02efb2ad8361259e512a14
carolineolagunju <olamidejr21@gmail.com>
2023-11-29 11:01:39 -0700

    use useEffect and useRef to reset selectedClient value

--- frontend/src/components/ClientSelection.jsx
+++ frontend/src/components/ClientSelection.jsx
@@ -1,10 +1,21 @@
 import "../styles/clientSelection.scss";
+import { useEffect, useRef } from "react";
 
-const ClientSelection = ({ handleClientSelect, clients }) => {
+const ClientSelection = ({ selectedClient, handleClientSelect, clients }) => {
+  const selectRef = useRef();
+  useEffect(() => {
+    if (selectedClient === null) {
+      selectRef.current.value = ""; // reset the select value
+    }
+  }, [selectedClient]);
+
   return (
     <div>
       <label className="select-client-label">Client List</label>
-      <select onChange={(e) => handleClientSelect(parseInt(e.target.value))}>
+      <select
+        ref={selectRef}
+         onChange={(e) => handleClientSelect(parseInt(e.target.value))}
+      >
         <option disabled selected value="">
           select a client
         </option>
