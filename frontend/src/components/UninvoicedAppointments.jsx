19c9c840d259de16316aca149a49c57b35fd3f0b
carolineolagunju <olamidejr21@gmail.com>
2023-12-01 02:46:15 -0700

    dead comments

--- frontend/src/components/UninvoicedAppointments.jsx
+++ frontend/src/components/UninvoicedAppointments.jsx
@@ -1,43 +1,3 @@
-// const AppointmentList = ({
-//   reviewedAppointments,
-//   handleAppointmentCheck,
-//   checkedAppointments,
-// }) => {
-//   const appointmentList = reviewedAppointments.map((appointment) => (
-//     <tr key={appointment.id} className="appointment-list-item">
-//       <td>{appointment.notes}</td>
-//       <td>{appointment.date}</td>
-//       <td>{appointment.confirmedHours} hours</td>
-//       <td>
-//         <input
-//           type="checkbox"
-//           checked={checkedAppointments.includes(appointment.id)}
-//           onChange={() => handleAppointmentCheck(appointment.id)}
-//         />
-//       </td>
-//     </tr>
-//   ));
-
-//   return (
-//     <div>
-//       <table className="appointment-list">
-//         <thead>
-//           <tr>
-//             <th>Appointment</th>
-//             <th>Date</th>
-//             <th>Hours</th>
-//             <th>Checkbox</th>
-//           </tr>
-//         </thead>
-//         <tbody>{appointmentList}</tbody>
-//       </table>
-//     </div>
-//   );
-// };
-
-// export default AppointmentList;
-
-
 import React from "react";
 import "../styles/uninvoicedAppointments.scss";
 
@@ -56,11 +16,6 @@ const UninvoicedAppointmentItem = ({
         <p>📆 {appointment.date}</p>
         <p>⏱ {appointment.confirmedHours} hours</p>
       </div>
-      {/* <input
-        type="checkbox"
-        checked={isChecked}
-        onChange={() => handleAppointmentCheck(appointment.id)}
-      /> */}
     </div>
   );
 };
